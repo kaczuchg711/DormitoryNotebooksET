db16d062ea010a5bcd3e591f9d426d16d6b4545e
Tomasz Kacza <kaczatom4@gmail.com>
2020-08-08 22:16:12 +0200

    add dorms and associative tabke

--- security/models.py
+++ security/models.py
@@ -6,3 +6,10 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=60)
     acronym = models.CharField(max_length=10)
+
+class Dorm(models.Model):
+    name = models.CharField(max_length=60)
+
+class Dorm_associative_Oraganaizor(models.Model):
+    id_dorm = models.ForeignKey(Dorm, on_delete=models.SET(0))
+    id_organization = models.ForeignKey(Organization, on_delete=models.SET(0))
