e8e5defbe1fd3515dacfdb229a9ae6100ec1dcb1
Tomasz Kacza <kaczatom4@gmail.com>
2020-12-01 21:46:19 +0100

    finish _data_ok with BlockedUsers

--- global_fun.py
+++ global_fun.py
@@ -1,5 +1,8 @@
+import time as basic_time
+
 def change_QuerySet_from_db_to_list(querySet, column_name):
     return [i[column_name] for i in [j for j in querySet]]
+
 
 # def change_QuerySet_from_db_to_list(querySet,atribute):
 #     entry_list = list(querySet)
@@ -7,6 +10,12 @@ def change_QuerySet_from_db_to_list(querySet, column_name):
 #             print(item.atribute)
 
 def print_with_enters(*valuses):
-    print("\n"*3)
+    print("\n" * 3)
     print(*valuses)
-    print("\n"*3)
+    print("\n" * 3)
+
+
+def get_actual_time():
+    t = basic_time.localtime()
+    actual_time = basic_time.strftime("%H:%M:%S", t)
+    return actual_time
--- security/views.py
+++ security/views.py
@@ -1,4 +1,4 @@
-from datetime import time, timedelta
+
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
@@ -10,7 +10,6 @@ from ipware import get_client_ip
 from global_fun import print_with_enters
 from organizations.models import Organization, Dorm
 from organizations import views as organizationsView
-from rental.models.NonDBmodels.BlockedUserSetter import BlockedUserSetter
 
 from security.forms import LoginForm
 from security.models.fun import create_user_to_log_in
@@ -67,49 +66,42 @@ def _get_authenticate_user(request):
 
 def _data_ok(request, user: User):
 
-    import time as timep
     client_ip, is_routable = get_client_ip(request)
 
-    blockedUserSetter = BlockedUserSetter(client_ip)
-    t = timep.localtime()
-    actual_time = timep.strftime("%H:%M:%S", t)
-    rows = BlockedUsers.objects.filter(ip=client_ip)
-    # check user ip is in blocked list and check that user have to wait
-    # check blocked time passed
-    if blockedUserSetter.have_user() and blockedUserSetter.user_is_blocked():
-        print_with_enters(blockedUserSetter.user_is_blocked())
-        if not blockedUserSetter.block_time_passed():
+    try:
+        blockedUser = BlockedUsers.objects.filter(ip=client_ip)[0]
+    except IndexError:
+        blockedUser = None
+
+    if blockedUser is not None and blockedUser.blocked:
+        if not blockedUser.block_time_passed():
             messages.add_message(request, messages.INFO, "too many attempts wait a moment")
             return False
         else:
-            blockedUserSetter.delete_user_from_blocked_list()
-    # todo clean this mebe simble change row[0] to object?
-    if user is not None:
-        if user.is_superuser:
-            dormName = request.POST['dorms']
-            request.session['dorm_id'] = Dorm.objects.filter(name=dormName)[0].id
-            return True
+            blockedUser.delete_user_from_blocked_list()
 
+    if user is not None:
         dormName = request.POST['dorms']
         request.session['dorm_id'] = Dorm.objects.filter(name=dormName)[0].id
+        if user.is_superuser:
+            return True
+
         organizationId = request.session.get("organization_id")
         try:
             LoginUser = create_user_to_log_in(user)
         except ValueError:
             messages.add_message(request, messages.INFO, "wrong data")
-            #
             return False
+
         if LoginUser.check_requirement(user, organizationId, dormName):
             return True
-    messages.add_message(request, messages.INFO, "wrong data")
 
-    if len(rows) == 0:
-        user_to_block = BlockedUsers(login=request.POST['email'], count=1, ip=client_ip, blocked=False, blocking_time=actual_time)
-        user_to_block.save()
+
+    if blockedUser is None:
+        BlockedUsers.create_blocked_user(request)
     else:
-        rows[0].count = str(int(rows[0].count) + 1)
-        if rows[0].count == "3":
-            rows[0].blocked = True
-            rows[0].blocking_time = actual_time
-        rows[0].save()
+        blockedUser.increase_number_of_wrong_attempts()
+        blockedUser.check_count_breach_and_block()
+
+    messages.add_message(request, messages.INFO, "wrong data")
     return False
